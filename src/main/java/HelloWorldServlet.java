817d59d4e95a1875b028b16319c8c5723ddb737f
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-08 14:20:25 -0500

    "finished servlets exercise"

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,7 +12,22 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
+        String name = req.getParameter("name");
+        if (name != null){
+            res.getWriter().println("<h1>Hello, " + name);
+        } else {
+            res.getWriter().println("Hello, World");
+        }
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello, World!</h1>");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Welcome, " + name + ":)</h1>");
+        out.println("<h3>Email: " + email + "</h3>");
     }
 }
