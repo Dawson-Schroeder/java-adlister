d2ad78c1076e394907844d661067793e0d578509
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-12 15:01:40 -0500

    finished exercise cookies

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -21,11 +21,13 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            session.setAttribute("username", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
