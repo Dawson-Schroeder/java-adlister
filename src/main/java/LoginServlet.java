1b879291a24e860b8a44093b519b002029d9efa1
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-12 14:42:00 -0500

    sessions and cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,11 +3,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        boolean isLoggedIn = session.getAttribute("username") != null;
+        if (!isLoggedIn ){
+            response.sendRedirect("/profile");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -22,4 +29,6 @@ public class LoginServlet extends HttpServlet {
             response.sendRedirect("/login");
         }
     }
+
+
 }
--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        boolean isLoggedIn = (Boolean) session.getAttribute("username") != null;
+        if (!isLoggedIn ){
+            response.sendRedirect("/login");
+            return;
+        }
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
