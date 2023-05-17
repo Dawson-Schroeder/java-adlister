b64d73e0ab548ad1f5ef931bf7b73878022d2c7d
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-17 13:28:20 -0500

    finished user-input exercise

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class MySQLUsersDao implements Users {
 
-    private Connection connection = null;
+    private Connection connection;
 
     public MySQLUsersDao(Config config) {
         try {
